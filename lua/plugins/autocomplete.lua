2fcd1ca7f76ae87efb77b4c99b6d3a066d3ce4bd
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2024-01-29 21:04:32 +0100

    add luasnip bc it was complaining without it

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,4 +1,10 @@
 return {
+ {'L3MON4D3/LuaSnip',
+  dependencies = {
+    'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets'
+    }
+  },
   {'hrsh7th/cmp-nvim-lsp'},
   {
     "hrsh7th/nvim-cmp",

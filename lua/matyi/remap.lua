34d63017f4be9558db403858c4817412e869f8a5
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2024-02-02 10:22:44 +0100

    remap vertical navigation between panes

--- lua/matyi/remap.lua
+++ lua/matyi/remap.lua
@@ -2,5 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>l", "<C-w>l")
 vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
 
 
